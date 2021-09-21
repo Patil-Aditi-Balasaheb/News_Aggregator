2db4d7030344b0aa51ae20c15c19acf6023fc63b
Aditi Balasaheb Patil <52288839+Patil-Aditi-Balasaheb@users.noreply.github.com>
2021-09-21 23:38:01 +0530

    Add files via upload

---
+++ UTMOST_NEWSAGGREGATOR/UTMOST_NEWSAGGREGATOR/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for UTMOST_NEWSAGGREGATOR project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UTMOST_NEWSAGGREGATOR.settings')
+
+application = get_asgi_application()
